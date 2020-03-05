188167c95e151b75a7490c0a208b8944770675ac
met4000 <nathsharwor@hotmail.com>
2020-03-05 19:49:40 +0800

    Add deadzone check to drivebase

--- 8035/src/main/cpp/strategies/drivetrain/Manual.cpp
+++ 8035/src/main/cpp/strategies/drivetrain/Manual.cpp
@@ -8,6 +8,9 @@ void Manual::OnUpdate(double dt) {
   double left = _contGroup->Get(ControlMap::Drivebase::LEFT);
   double right = _contGroup->Get(ControlMap::Drivebase::RIGHT);
 
+  if (std::abs(left) <= ControlMap::AXIS_DEADZONE) left = 0;
+  if (std::abs(right) <= ControlMap::AXIS_DEADZONE) right = 0;
+
   _drivetrain->Set(
     left * std::fabs(left) * ControlMap::Drivebase::THROTTLE,
     right * std::fabs(right) * ControlMap::Drivebase::THROTTLE
